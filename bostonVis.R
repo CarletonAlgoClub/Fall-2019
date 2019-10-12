758f791771ee25fca3019378675212480be270ee
epickens <e99pickens@gmail.com>
2019-10-12 16:12:52 -0500

    vis file

---
+++ bostonVis.R
@@ -0,0 +1,29 @@
+library(MASS)
+library(tidyverse)
+
+boston <- as.data.frame(Boston)
+
+boston %>% 
+  ggplot(aes(rm, medv, color=crim, size=crim)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+basic.model <- lm(data = boston, medv ~ rm)
+
+summary(basic.model)
+
+boston %>% 
+  ggplot(aes(crim)) +
+  geom_density() 
+
+boston %>% 
+  ggplot(aes(log(crim))) +
+  geom_density() 
+
+boston %>% 
+  ggplot(aes(rm)) +
+  geom_density() 
+
+log.model <- lm(data=boston, medv ~ rm + log(crim))
+
+summary(log.model)
